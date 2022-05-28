409aeae0c8f9e83888f9b53714da0b7eabb5d394
JoniPrifti <joni.prifti@fshnstudent.info>
2022-05-28 02:01:43 +0200

    ...

---
+++ Test/Attributes/ExeptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDetyr4.Attributes
+{
+    public class ExeptionAttribute : ActionFilterAttribute, IExceptionFilter
+    {
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (!filterContext.ExceptionHandled &&
+             filterContext.Exception is ArgumentOutOfRangeException)
+            {
+                filterContext.Result
+                = new RedirectResult("~/Home/ErrorPage");
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DockerDetyr4.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ExeptionAttribute]
+        public string RangeError(int id)
+        {
+            if (id > 100)
+            {
+                return String.Format("The id value is:(0)", id);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("id", id, "");
+            }
+        }
+        public IActionResult ErrorPage()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
